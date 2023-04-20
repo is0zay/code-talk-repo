74f48605281abc9bad7ba114fbfb3097c8ac63df
Isaiah <isozay0@gmail.com>
2023-04-20 16:03:20 -0400

    adding 1st code talk to repo

---
+++ codetalk1.js
@@ -0,0 +1,8 @@
+let y = 0;              // The initial bvalue of the y variable is set to 0
+let x = 0;              // the initial value of the x value is set to 0
+while (y < 4) {         //while loop, loops through a block of code {what is between the brackets} as long as a certain condition is true (what is in the parenthesis)
+  y++;                  //  start by increasing the value by 1
+  x += y;               // Then add the new value of y to the x variable;
+  
+  console.log(x)
+}
